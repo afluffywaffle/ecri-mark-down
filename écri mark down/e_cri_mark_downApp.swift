b82d834fcc8539e4411fe60c27b13c713d5d579f
Jayrom Acorda <jayrom.acorda@icloud.com>
2026-06-18 14:24:49 -0700

    Initial Commit

---
+++ écri mark down/e_cri_mark_downApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_cri_mark_downApp.swift
+//  écri mark down
+//
+//  Created by Jayrom Acorda on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct e_cri_mark_downApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
